4fa30069c25053d462ea2649c85e7d07e7165e04
viralsir <viralsir2018@gmail.com>
2021-04-01 10:17:57 +0530

    Datatable html file is modified. entry and view functionality is completed.

--- course/urls.py
+++ course/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import NewCourse
+from .views import NewCourse,ViewCourse
 urlpatterns=[
-    path("new/",NewCourse.as_view(),name="course-new")
+    path("new/",NewCourse.as_view(),name="course-new"),
+    path("view/",ViewCourse.as_view(),name="course-view")
 
 ]
